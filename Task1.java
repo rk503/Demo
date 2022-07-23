5e96d219ea1402443ac47f587d5473e24ade300e
rk503 <rajrk6606@gmail.com>
2022-07-23 23:16:24 +0530

    comments

--- Task1.java
+++ Task1.java
@@ -3,32 +3,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Task3 {
+public class Task1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		
-	/*	QUESTION 3
-		-----------
-		URL : http://demo.automationtesting.in/Register.html
-
-		NOTE: Give details and register the form.
-	*/	
 		System.setProperty("webdriver.chrome.driver","D:\\java tools\\chromedriver.exe");
 
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("http://demo.automationtesting.in/Register.html");
+		//question1
+		//hi
+	//	URL : https://www.amazon.in/
+//		NOTE: Click start here. Enter iphone and search
+//kklp
+		
+		WebDriver driver = new ChromeDriver();;
+		driver.get("https://www.amazon.in/");;
+		
+		driver.get("https://www.amazon.in/");;
 		driver.manage().window().maximize();
 		WebElement txtsrch= driver.findElement(By.id("twotabsearchtextbox"));
-		txtsrch.sendKeys("iphone");
+		txtsrch.sendKeys("iphonee");
 		WebElement btnclk =driver.findElement(By.id("nav-search-submit-button"));
-		btnclk.click();
-
-		
-		
-		
-			}
+		btnclk.click();;
+System.out.println("jii");
+System.out.println("oo");
+	}
 
 }
+  
